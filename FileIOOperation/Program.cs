1e634f55a0e9012318376920cb323052f3a62a6e
Jebakani <vjishwarya@gmail.com>
2021-07-25 17:46:10 +0530

    [JEBAKANI] Creating the binary file using binary serialization

--- FileIOOperation/Program.cs
+++ FileIOOperation/Program.cs
@@ -7,13 +7,8 @@ namespace FileIOOperation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to File IO Operation");
-            string filepath = @"C:\Users\HP1\Desktop\TestFiles.txt";
-            string outputFile = @"C:\Users\HP1\source\repos\Lambda\New folder\FileIOOperation\TestFile1.txt";
-            BasicOperation.FileExists(filepath);
-            BasicOperation.ReadAllLine(filepath);
-            BasicOperation.ReadFile(filepath);
-            BasicOperation.ReadFromStreamReader(filepath);
-            BasicOperation.WriteUsingStreamWriter(filepath);
+            SearializationAndDeserialization operation = new SearializationAndDeserialization();
+            operation.BinarySerialization();
             Console.Read();
         }
         
---
+++ FileIOOperation/SearializationAndDeserialization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperation
+{
+    class SearializationAndDeserialization
+    {
+        //converting the object into the binary format
+        public void BinarySerialization()
+        {
+            string path = @"C:\Users\HP1\source\repos\Lambda\New folder\FileIOOperation\Binary.txt";
+            //creating the object for the personData
+            PersonData data = new PersonData();
+            data.name = "Jeba";
+            data.age = 21;
+            data.name = "acx";
+            data.age = 31;
+            //creating the stream for the file to write
+            FileStream file = File.OpenWrite(path);
+            //it convert the object into binary format
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(file, data);
+        }
+    }
+}
